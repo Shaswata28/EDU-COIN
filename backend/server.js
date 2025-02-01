0a03077d5aa2d27a2b6c053a84aa46bfad771523
Shaswata Das <arnobsps@gmail.com>
2025-02-01 23:54:20 +0600

    set server port to use environment variable or default to 5000

--- backend/server.js
+++ backend/server.js
@@ -90,7 +90,7 @@ app.put('/api/profile/password', protect, updatePassword);
 // Message Routes
 app.use('/api/messages', messageRoutes);
 
-const PORT =  5100;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
