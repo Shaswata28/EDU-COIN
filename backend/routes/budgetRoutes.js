81e91ec6cededf50fc595d8cc4d418cc3b9972ed
Shaswata Das <145161471+Shaswata28@users.noreply.github.com>
2025-05-08 20:31:11 +0600

    Update budgetRoutes.js

--- backend/routes/budgetRoutes.js
+++ backend/routes/budgetRoutes.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import { protect } from '../middleware/authMiddleware.js';
-import { updateBudget, getBudgets } from '../controllers/budgetController.js';
+import { 
+  updateBudget, 
+  getBudgets,
+  getMonthlyReport 
+} from '../controllers/budgetController.js';
 
 const router = express.Router();
 
 router.get('/', protect, getBudgets);
 router.put('/', protect, updateBudget);
+router.get('/report', protect, getMonthlyReport);
 
 export default router;
